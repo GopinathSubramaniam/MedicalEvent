3b967ec087cc9472f9715f7efb838126cc6aba19
Gopi <gopiwrld@gmail.com>
2020-02-06 19:06:06 +0530

    Responsive and UI alignment

--- src/app/app.conf.ts
+++ src/app/app.conf.ts
@@ -2,7 +2,7 @@ export const appConf = {
   skin: 'blue',
   sidebarLeftMenu: [
     { label: 'Dashboard', route: '/', iconClasses: 'fa fa-dashboard' },
-    { label: 'Events', route: 'events', iconClasses: 'fa fa-files-o' },
+    { label: 'Events', route: 'events', iconClasses: 'fa fa-calendar-o' },
     { label: 'Guest Users', route: 'user/guestList', iconClasses: 'fa fa-users' },
     { label: 'Organisers', iconClasses: 'fa fa-users' },
     { label: 'NewsLetters', route: 'newsletter', iconClasses: 'fa fa-newspaper-o' },
